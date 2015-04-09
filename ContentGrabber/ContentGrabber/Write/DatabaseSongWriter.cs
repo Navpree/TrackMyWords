21c4f9233b574bacb643c7c6f3b622c43e6576b4
pele1250 <canadianmadebro@yahoo.ca>
2015-04-08 20:51:32 -0400

    Updated test suite, proposed fix for duplicate albums from generated ContentGrabber queries.

--- ContentGrabber/ContentGrabber/Write/DatabaseSongWriter.cs
+++ ContentGrabber/ContentGrabber/Write/DatabaseSongWriter.cs
@@ -41,8 +41,9 @@ namespace ContentGrabber.Write
             //lyrics.Replace("'", @"''");
             //album.Replace("'", @"''");
             //artist.Replace("'", @"''");
+            //insert into Album(title, cover, release_date) select * from (select ?, ?, ?) as tmp where not exists(select title from Album where title = ?) limit 1;
             query.WriteLine(string.Format("insert into Song (title, lyrics, release_date) values ('{0}', '{1}', {2});", song.Replace("'", @"''"), lyrics.Replace("'", @"''"), "2012-12-12"));
-            query.WriteLine(string.Format("insert into Album(title, cover, release_date) values (\'{0}\', \'{1}\', {2});", album.Replace("'", @"''"), "null", "2012-12-12"));
+            query.WriteLine(string.Format("insert into Album(title, cover, release_date) select * from (select '{0}', '{1}', '{2}') as tmp where not exists(select title from Album where title = '{0}') limit 1;", album.Replace("'", @"''"), "null", "2012-12-12"));
             query.WriteLine(string.Format("insert into Artist (artist_name) select * from (select \'{0}\') as tmp where not exists(select artist_name from Artist where artist_name = \'{0}\') limit 1;", artist.Replace("'", @"''")));
             query.WriteLine(string.Format("insert into Song_Album (song_id, album_id) select song_id, album_id from Song, Album where Song.title = \'{0}\' and Album.title = \'{1}\';", song.Replace("'", @"''"), album.Replace("'", @"''")));
             query.WriteLine(string.Format("insert into Artist_Album (album_id, artist_id) select album_id, artist_id from Album, Artist where Album.title = \'{0}\' and Artist.artist_name = \'{1}\';", album.Replace("'", @"''"), artist.Replace("'", @"''")));
